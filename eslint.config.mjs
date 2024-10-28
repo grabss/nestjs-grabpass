220784c666b319f60534bd1f4775e402e91514c6
irohafox <matsukawa@grabss.co.jp>
2024-10-28 16:01:45 +0900

    Chore: eslint-plugin-import & eslint-plugin-unused-imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
-import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
-import globals from 'globals'
-import tsParser from '@typescript-eslint/parser'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
+
 import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import importPlugin from 'eslint-plugin-import'
+import unusedImportPlugin from 'eslint-plugin-unused-imports'
+import globals from 'globals'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -24,11 +27,14 @@ export default [
   ),
   {
     plugins: {
-      '@typescript-eslint': typescriptEslintPlugin
+      '@typescript-eslint': typescriptEslintPlugin,
+      import: importPlugin,
+      'unused-imports': unusedImportPlugin
     },
     languageOptions: {
       globals: {
-        ...globals.node
+        ...globals.node,
+        ...globals.jest
       },
       parser: tsParser,
       ecmaVersion: 5,
@@ -41,7 +47,37 @@ export default [
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off'
+      '@typescript-eslint/no-explicit-any': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'object',
+            'type',
+            'index'
+          ],
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          }
+        }
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_'
+        }
+      ]
     }
   }
 ]
