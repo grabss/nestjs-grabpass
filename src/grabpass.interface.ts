c6eb86be049f28be9aa8410f5cf3469a7c2f6be7
irohafox <matsukawa@grabss.co.jp>
2024-10-28 16:27:09 +0900

    WIP

---
+++ src/grabpass.interface.ts
@@ -0,0 +1,3 @@
+export type GrabpassModuleOptions = {
+  todo: string
+}
--- src/grabpass.module.ts
+++ src/grabpass.module.ts
@@ -1,11 +1,20 @@
-import { DynamicModule, Module } from '@nestjs/common'
+import { DynamicModule, Module, Provider } from '@nestjs/common'
+
+import { GrabpassModuleOptions } from './grabpass.interface'
+import { GrabpassService } from './grabpass.service'
 
 @Module({})
 export class GrabpassModule {
-  static forRoot(): DynamicModule {
+  static forRoot(options: GrabpassModuleOptions): DynamicModule {
+    const grabpassOptionsProvider: Provider = {
+      provide: 'GRABPASS_MODULE_OPTIONS',
+      useValue: options
+    }
+
     return {
       module: GrabpassModule,
-      providers: []
+      providers: [grabpassOptionsProvider, GrabpassService],
+      exports: [GrabpassService]
     }
   }
 }
