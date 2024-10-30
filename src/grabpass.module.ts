38eb2174445c021e98701eb70933fb74ce9e7827
irohafox <matsukawa@grabss.co.jp>
2024-10-30 15:28:25 +0900

    WIP: options

--- src/grabpass.module.ts
+++ src/grabpass.module.ts
@@ -1,7 +1,6 @@
 import { DynamicModule, Module, Provider } from '@nestjs/common'
-import { Grabpass } from 'grabpass'
 
-import { GRABPASS, GRABPASS_MODULE_OPTIONS } from './grabpass.constants'
+import { GRABPASS_MODULE_OPTIONS } from './grabpass.constants'
 import { GrabpassModuleOptions } from './grabpass.interface'
 import { GrabpassService } from './grabpass.service'
 
@@ -16,19 +15,9 @@ export class GrabpassModule {
       useValue: options
     }
 
-    const grabpassProvider: Provider = {
-      provide: GRABPASS,
-      useFactory: (options: GrabpassModuleOptions) => {
-        return new Grabpass({
-          config: options.config
-        })
-      },
-      inject: [GRABPASS_MODULE_OPTIONS]
-    }
-
     return {
       module: GrabpassModule,
-      providers: [grabpassModuleOptionsProvider, grabpassProvider]
+      providers: [grabpassModuleOptionsProvider]
     }
   }
 }
