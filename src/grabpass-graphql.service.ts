ae2bc4450cfa56260a9a3aee0ab1e7c39ca5e70d
irohafox <matsukawa@grabss.co.jp>
2024-11-01 17:08:32 +0900

    WIP: AuthContext

--- src/grabpass-graphql.service.ts
+++ src/grabpass-graphql.service.ts
@@ -1,7 +1,23 @@
-import { Inject, Injectable } from '@nestjs/common'
+import {
+  createParamDecorator,
+  Inject,
+  Injectable,
+  type ExecutionContext
+} from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
 import { Grabpass } from 'grabpass'
 
 import { GRABPASS } from './grabpass.constants'
+
+const GRABPASS_AUTH_CONTEXT = 'GRABPASS_AUTH_CONTEXT'
+
+export const AuthContext = createParamDecorator(
+  (_, context: ExecutionContext) => {
+    return GqlExecutionContext.create(context).getContext().req[
+      GRABPASS_AUTH_CONTEXT
+    ]
+  }
+)
 
 @Injectable()
 export class GrabpassGraphqlService {
